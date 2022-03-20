2438b5f2509bfc70ae62fefb994a3ec6d12447ac
Koki Iijima <koki.iijima@outlook.jp>
2022-03-20 13:11:20 +0900

    test commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import API from './api/api';
+import API from './api/api'
 
 function App() {
-
-  API.get(`recipe/list`)
-  .then(res => {
-    console.log(res);
+  API.get(`recipe/list`).then((res) => {
+    console.log(res)
   })
 
   return (
-    <div className="App">
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='App'>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
